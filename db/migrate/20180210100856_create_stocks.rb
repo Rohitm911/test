9355a684659039366c4bb8f0f3b518946b05f93a
Rohit <rohitmehra.cs17@bmsce.acin>
2018-02-10 18:12:43 +0530

    Some testing

---
+++ db/migrate/20180210100856_create_stocks.rb
@@ -0,0 +1,11 @@
+class CreateStocks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :stocks do |t|
+      t.string :name
+      t.string :ticker
+      t.float :price
+
+      t.timestamps
+    end
+  end
+end
